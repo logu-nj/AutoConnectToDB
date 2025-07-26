04cf38853125d9813dc6688de17a767cfc1508db
aswin-dotcom <aswin42samuel7@gmail.com>
2025-07-26 18:07:23 +0530

    FIX Auto Updated

---
+++ AutoConnectToDB/Migrations/20250726123516_V-0.2.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AutoConnectToDB.Migrations
+{
+    /// <inheritdoc />
+    public partial class V02 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PhoneNo",
+                table: "Students",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PhoneNo",
+                table: "Students");
+        }
+    }
+}
